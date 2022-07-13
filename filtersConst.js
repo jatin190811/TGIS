8e8ea10e81766a1e4a001b6dc31dee5d1d171e0f
Jatin <jatinarora782@gmail.com>
2022-07-13 16:40:12 +0530

    added filter key

--- filtersConst.js
+++ filtersConst.js
@@ -1,109 +1,114 @@
 module.exports = {
     venues : {
         sub_cat : ['Banquet','Lawn','Farm House','Resort','Pary Hall'],
-        noOfGuests : [
-            'Less than 100',
-            '100-250',
-            '250-500',
-            '500-1000',
-            'More than 1000'
-        ],
-        venueType :[
-            '4 star & above',
-            'Banquet Hall',
-            'Farmhouse',
-            'Golf club'
-        ],
-        spaceAndDecor : [
-            'Indoor',
-            'Outdoor',
-            'Poolside',
-            'Terrace'
-        ],
-        pricePerPlate : [
-            'lessThan 1000',
-            'Rs 1000 - 1500',
-            'Rs 1500 - 2000',
-            'Rs 2000 - 3000',
-            'Rs 3000 & above'
-        ],
-        menuType : ['Veg', 'Non veg']
+        filters: {
+            noOfGuests : [
+                'Less than 100',
+                '100-250',
+                '250-500',
+                '500-1000',
+                'More than 1000'
+            ],
+            venueType :[
+                '4 star & above',
+                'Banquet Hall',
+                'Farmhouse',
+                'Golf club'
+            ],
+            spaceAndDecor : [
+                'Indoor',
+                'Outdoor',
+                'Poolside',
+                'Terrace'
+            ],
+            pricePerPlate : [
+                'lessThan 1000',
+                'Rs 1000 - 1500',
+                'Rs 1500 - 2000',
+                'Rs 2000 - 3000',
+                'Rs 3000 & above'
+            ],
+            menuType : ['Veg', 'Non veg']
+        }
+        
 
     },
 
     bridalMakeups : {
-        pricePerEvent : [
-            'Less Than 12K',
-            '12k-16k',
-            '16k-20k',
-            '20k-25k',
-            'More than 25K'
-        ],
-        speciality : [
-            'North Indian',
-            'South Indian',
-            'Bengali',
-            'Muslims',
-            'Marathi',
-            'Punjabi'
-        ],
-        travelToVenue: [
-            'Travel to Venue',
-            'Does\'nt travel' 
-        ],
-        airbrudh : [
-            'Offers HD',
-            'Offers airbrush'
-        ],
-        ratings :[
-            'all ratings',
-            'Rated 4.5+',
-            'Rated 4+',
-            'Rated <4',
-        ],
-        vaccinated : [
-            'vaccinated'
-        ]
+        filters : {
+            pricePerEvent : [
+                'Less Than 12K',
+                '12k-16k',
+                '16k-20k',
+                '20k-25k',
+                'More than 25K'
+            ],
+            speciality : [
+                'North Indian',
+                'South Indian',
+                'Bengali',
+                'Muslims',
+                'Marathi',
+                'Punjabi'
+            ],
+            travelToVenue: [
+                'Travel to Venue',
+                'Does\'nt travel' 
+            ],
+            airbrudh : [
+                'Offers HD',
+                'Offers airbrush'
+            ],
+            ratings :[
+                'all ratings',
+                'Rated 4.5+',
+                'Rated 4+',
+                'Rated <4',
+            ],
+            vaccinated : [
+                'vaccinated'
+            ]
+        }
     },
 
     bridalWears : {
         sub_cat : ['Kanjeevaram Sarees','Silk Sarees','Bridal Lehengas','Resort','Cocktail Gowns'],
-        pricePerEvent : [
-            'Less Than 12K',
-            '12k-16k',
-            '16k-20k',
-            '20k-25k',
-            'More than 25K'
-        ],
-        storeType:[
-            'Retail Shop',
-            'Studio / Boutique',
-            'Couture Brand',
-            'Multi Design Studio',
-            'rental Store'
-        ],
-        outfit : [
-            'Bridal Lehengas',
-            'Light Lehengas',
-            'Trousseau Sarees',
-            'Cocktail Gowns'
-        ],   
-        Deals : ['Mynt'],
-        ratings :[
-            'all ratings',
-            'Rated 4.5+',
-            'Rated 4+',
-            'Rated <4',
-        ],
-        vaccinated : [
-            'vaccinated'
-        ]
-
+        filters : {
+            pricePerEvent : [
+                'Less Than 12K',
+                '12k-16k',
+                '16k-20k',
+                '20k-25k',
+                'More than 25K'
+            ],
+            storeType:[
+                'Retail Shop',
+                'Studio / Boutique',
+                'Couture Brand',
+                'Multi Design Studio',
+                'rental Store'
+            ],
+            outfit : [
+                'Bridal Lehengas',
+                'Light Lehengas',
+                'Trousseau Sarees',
+                'Cocktail Gowns'
+            ],   
+            Deals : ['Mynt'],
+            ratings :[
+                'all ratings',
+                'Rated 4.5+',
+                'Rated 4+',
+                'Rated <4',
+            ],
+            vaccinated : [
+                'vaccinated'
+            ]
+        }
     },
 
     photographers : {
-        city : '',
-        noOfDays : [
+        filters: {noOfDays : [
             '1 day',
             '2 days',
             '3 days',
@@ -130,79 +135,83 @@ module.exports = {
         ],
         vaccinated : [
             'vaccinated'
-        ]
+        ]}
     },
 
     groomWear  : {
-        city : '',
-        outfitType :[
-            'sherwani',
-            'wedding suite',
-            'Kurta Pyjama',
-            'Bandhgala'
-        ],
-        accessories : [
-            'safa for groom',
-            'Groom pagri'
-        ],
-        price :[
-            'Less than 5000',
-            '5000 - 10000',
-            '10000 - 15000',
-            '15000 - 20000',
-            'More than 20000'
-        ],
-        Deals : ['Mynt'],
-        ratings :[
-            'all ratings',
-            'Rated 4.5+',
-            'Rated 4+',
-            'Rated <4',
-        ],
-        vaccinated : [
-            'vaccinated'
-        ]
+        filters : {
+            outfitType :[
+                'sherwani',
+                'wedding suite',
+                'Kurta Pyjama',
+                'Bandhgala'
+            ],
+            accessories : [
+                'safa for groom',
+                'Groom pagri'
+            ],
+            price :[
+                'Less than 5000',
+                '5000 - 10000',
+                '10000 - 15000',
+                '15000 - 20000',
+                'More than 20000'
+            ],
+            Deals : ['Mynt'],
+            ratings :[
+                'all ratings',
+                'Rated 4.5+',
+                'Rated 4+',
+                'Rated <4',
+            ],
+            vaccinated : [
+                'vaccinated'
+            ]
+        }
     },
 
     mehndi : {
-        city : '',
-        price :[
-            'Less than 5000',
-            '5000 - 10000',
-            '10000 - 15000',
-            '15000 - 20000',
-            'More than 20000'
-        ],
-        Deals : ['Mynt'],
-        ratings :[
-            'all ratings',
-            'Rated 4.5+',
-            'Rated 4+',
-            'Rated <4',
-        ],
-        vaccinated : [
-            'vaccinated'
-        ]
+        filters : {
+            price :[
+                'Less than 5000',
+                '5000 - 10000',
+                '10000 - 15000',
+                '15000 - 20000',
+                'More than 20000'
+            ],
+            Deals : ['Mynt'],
+            ratings :[
+                'all ratings',
+                'Rated 4.5+',
+                'Rated 4+',
+                'Rated <4',
+            ],
+            vaccinated : [
+                'vaccinated'
+            ]
+        }
     },
 
     planner : {
         sub_cat: ['Wedding Planners', 'Decorations'],
-        price :[
-            'Less Than 12K',
-            '12k-16k',
-            '16k-20k',
-            '20k-25k',
-            'More than 25K'
-        ],
-        ratings :[
-            'all ratings',
-            'Rated 4.5+',
-            'Rated 4+',
-            'Rated <4',
-        ],
-        vaccinated : [
-            'vaccinated'
-        ]
+        filters : {
+            price :[
+                'Less Than 12K',
+                '12k-16k',
+                '16k-20k',
+                '20k-25k',
+                'More than 25K'
+            ],
+            ratings :[
+                'all ratings',
+                'Rated 4.5+',
+                'Rated 4+',
+                'Rated <4',
+            ],
+            vaccinated : [
+                'vaccinated'
+            ]
+        } 
     },
 
 
