5249a9c5d5abdc404dba163c7702ba613aface4f
Jatin <jatinarora782@gmail.com>
2022-08-21 18:54:03 +0530

    user change

--- controllers/user.js
+++ controllers/user.js
@@ -303,7 +303,17 @@ async function verifyonboarding(req, res) {
     let collection = await client.db("admin").collection('vendors');
     let result = await collection.updateOne({ $and: [{ _id: new ObjectId(ref) }, { otp: otp }] }, { $set: { 'active': true } })
     if (result.modifiedCount) {
-        return res.json({ status: 'success', message: 'Account Successfully created', data: {} })
+
+
+        let cursor = collection.find({ _id: new ObjectId(ref) })
+        let user = await cursor.toArray()
+        let token = jwt.sign({ name: user[0]['name'], id: user[0]['_id'] }, 'P!yush@1994');
+        
+
+        return res.json({ status: 'success', message: 'Account Successfully created', data: {
+            token,
+            profile : user[0]
+        } })
     } else {
         return res.json({ status: 'error', error: '026', message: 'Invalid Otp' })
     }
